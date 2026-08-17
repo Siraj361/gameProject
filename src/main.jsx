847dc3ab65bf6882f903793e5d5a7ad82622f881
Abdul Moiz <moiz@gmail.com>
2026-08-17 17:35:29 +0500

    Fix React Router navigation

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ registerSW({
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/gameProject">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
